cc63b63428ac81461c3f3bfe3fecd80707f8360a
imamagf <imamagfa06@gmail.com>
2020-06-30 11:15:54 +0700

    menambahkan 2 database

---
+++ latihan1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Daftar Mahasiswa</h3>
+
+
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>Nim</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+
+    <tr>
+      <td>1</td>
+      <td><img src=""></td>
+    </tr>
+  </table>
+</body>
+
+</html>
